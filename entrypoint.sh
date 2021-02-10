9ace26705d4d5c70909a6c069f396a2f8cecbf03
Kyle McNicoll <37396126+KMcNickel@users.noreply.github.com>
2021-02-09 19:20:37 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,4 +23,4 @@ chmod 600 "$SSH_PATH/deploy_key"
 
 
 # Do deployment
-sh -c "rsync $1 -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/$2 $3"
+sh -c "rsync INPUT_ARGS -e 'ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no' $GITHUB_WORKSPACE/INPUT_SOURCE INPUT_TARGET"
